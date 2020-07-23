4a40c116d011a9ad7dd47a79a7d1bf4d37554fb5
19h47 <jeremylevron@19h47.fr>
2020-07-23 10:21:15 +0200

    :sparkles: Add favicon to docs

--- config/webpack.development.js
+++ config/webpack.development.js
@@ -1,17 +1,13 @@
 /**
  *
  * @file webpack.development.js
- * @author Jérémy Levron <jeremylevron@19h47.fr> (http://19h47.fr)
+ * @author Jérémy Levron <jeremylevron@19h47.fr> (https://19h47.fr)
  */
 
 const path = require('path');
 
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-
-function resolve(dir) {
-	return path.join(__dirname, '..', dir);
-}
 
 module.exports = merge(common, {
 	mode: 'development',
