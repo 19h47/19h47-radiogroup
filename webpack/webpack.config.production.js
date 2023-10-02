5bbedccef15a9c70155306bfe7085db3f41e0632
19h47 <jeremylevron@19h47.fr>
2023-10-02 12:20:10 +0200

    :bulb: Fix doc favicon

--- webpack/webpack.config.production.js
+++ webpack/webpack.config.production.js
@@ -6,8 +6,17 @@
 
 console.log('[Webpack] webpack.production.js');
 
+const CopyPlugin = require('copy-webpack-plugin');
+
+const resolve = require('./webpack.utils');
+
 module.exports = {
 	mode: 'production',
 	devtool: false,
 	watch: false,
+	plugins: [
+		new CopyPlugin({
+			patterns: [{ from: resolve('radio-button.png'), to: resolve('docs/') }],
+		}),
+	],
 };
